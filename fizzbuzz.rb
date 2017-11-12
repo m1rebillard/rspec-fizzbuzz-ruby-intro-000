1ef72deeebeae12ec8081487538a1da8b03af8ba
Myra Rebillard <myra.rebillard@gmail.com>
2017-11-12 03:10:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
   string_return = ""
   if (number % 3 == 0)
-    string_return = "Fizz"
+    string_return.concat("Fizz")
   end
   if (number % 5 == 0)
-    string_return = "Buzz"
+    string_return.concat("Buzz")
   end
   puts string_return
 end
